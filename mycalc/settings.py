4aa2f5b846d6550fd5485ff5b39e26b881f735a4
NarroW12 <jakub.omie@gmail.com>
2025-10-09 17:32:53 +0200

    First Commit

--- mycalc/settings.py
+++ mycalc/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mpszmo*^cs$p&w81f4^=@_@ud#t-u*%%$8#3z=(g47n2@_c+8='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
