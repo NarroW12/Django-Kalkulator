b02a652d90a8470c1940a37a77c05d6d11409101
NarroW12 <jakub.omie@gmail.com>
2025-10-16 17:55:54 +0200

    poprawa settings

--- mycalc/settings.py
+++ mycalc/settings.py
@@ -11,15 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'calculator/static'),
-]
-
+STATIC_URL = 'static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
